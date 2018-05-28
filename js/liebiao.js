489317070e1d16f55564dbe657c16b7b0e4b951b
zhang heng <zhangheng@163.com>
2018-05-28 21:40:26 +0800

    4

---
+++ js/liebiao.js
@@ -0,0 +1,38 @@
+
+
+var oMenu=document.getElementById("menu_h");
+var oDl=document.getElementsByTagName("dl");
+var oMenus=document.getElementById("menu");
+var oMenu_div=document.getElementById("menu_div");
+oMenu_div.onmouseover=function(){
+	for (var i=0; i<oDl.length; i++) {
+		oDl[i].style.display="block";
+	}
+	
+}
+oMenu_div.onmouseout=function(){
+		
+	for (var i=0; i<oDl.length; i++) {
+		oDl[i].style.display="none";
+		//console.log("aaa");
+	}
+}
+var oShuliao=document.getElementById("shuliang");
+var sFlat=document.getElementById("flat_rght");
+var oFlat_span=document.getElementById("flat_span1");
+var Stop=0;
+window.onscroll=function(){
+	Stop=document.documentElement.scrollTop||document.body.scrollTop;
+	
+	if(Stop>400){
+		oShuliao.style.position="fixed";
+		sFlat.style.display="block";
+		//console.log("aa");
+		
+	}else{
+		oShuliao.style.position="relative";
+		sFlat.style.display="none";
+		
+	}
+	
+}
